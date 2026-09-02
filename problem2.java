dc8107d51b123c9a140744890d8e38e49b29135c
katravathpraveenkumar <pk6837152@gmail.com>
2026-09-02 23:43:24 +0530

    updated code

--- problem2.java
+++ problem2.java
@@ -1,3 +1,36 @@
-public abstract class problem2 {
-    
+public class problem2 {
+    public static class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    static Node head;
+
+    public static boolean isCycle(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head.next; // Creating a cycle for testing
+        System.out.println("Does the linked list have a cycle " + isCycle(head));
+    }
 }
+        
+
