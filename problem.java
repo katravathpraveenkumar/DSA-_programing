df0ca71b544546a7978e4983fcd9f51fcc5f5a0f
katravathpraveenkumar <pk6837152@gmail.com>
2026-09-02 23:03:09 +0530

    updated code

--- problem.java
+++ problem.java
@@ -1,12 +1,30 @@
+// public class problem{
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 4, 5};
+//         int sum = 0;
+
+//         for (int i = 0; i < arr.length; i++) {
+//             sum += arr[i];
+//         }
+
+//         System.out.println("The sum of the array elements is: " + sum);
+//     }
+// }
+
 public class problem{
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        int sum = 0;
+        int evenCount = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+            if (isEven(arr[i])) {
+                evenCount++;
+            }
         }
 
-        System.out.println("The sum of the array elements is: " + sum);
+        System.out.println("The number of even elements in the array is: " + evenCount);
     }
 }
